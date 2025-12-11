f5067478fa7de957ce1718793f921d628bd0097a
alireza moradi <alirezza.dev2@gmail.com>
2025-12-11 11:05:52 +0330

    1.responsive Header

--- src/component/modules/Navber/Navbar.tsx
+++ src/component/modules/Navber/Navbar.tsx
@@ -1,30 +1,61 @@
-import React from 'react'
+import React from "react";
 import { LuMoonStar } from "react-icons/lu";
+import { FaBars } from "react-icons/fa6";
 
 function Navbar() {
   return (
     <>
-      <nav className='absolute w-full flex justify-center z-50' >
-        <div className='fixed w-full mx-5 xl:w-[1240px] lg:w-[900px] md:w-[700px] bg-transparent md:flex items-center justify-between mt-5 backdrop-blur-xl p-2 rounded-2xl'>
-          <ul className='flex items-center justify-center gap-4 text-gray-300 '>
-            <li className='transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer'> خانه </li>
-            <li className='transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer'> پروژه </li>
-            <li className='transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer'> رزومه </li>
-            <li className='transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer'> وبلاگ </li>
-            <li className='transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer'> ارتباط با من </li>
-            <li className='transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer'> درباره من </li>
+      <nav className="absolute w-full flex justify-center z-50">
+        <div className="fixed w-full mx-5 xl:w-[1240px] lg:w-[900px] md:w-[700px] bg-transparent md:flex items-center justify-between mt-5 backdrop-blur-xl p-2 rounded-2xl">
+          <ul className="hidden  md:flex items-center justify-center gap-4 text-gray-300 ">
+            <li className="transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer">
+              {" "}
+              خانه{" "}
+            </li>
+            <li className="transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer">
+              {" "}
+              پروژه{" "}
+            </li>
+            <li className="transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer">
+              {" "}
+              رزومه{" "}
+            </li>
+            <li className="transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer">
+              {" "}
+              وبلاگ{" "}
+            </li>
+            <li className="transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer">
+              {" "}
+              ارتباط با من{" "}
+            </li>
+            <li className="transition-all hover:text-gray-200 active:text-gray-200 cursor-pointer">
+              {" "}
+              درباره من{" "}
+            </li>
           </ul>
-          <div className='flex items-center justify-center gap-28'>
+          <div className="hidden md:flex items-center justify-center gap-5 xl:gap-28">
             <button>
-              <LuMoonStar className='text-gray-600 text-2xl transition-all hover:text-white cursor-pointer' />
+              <LuMoonStar className="text-gray-600 text-2xl transition-all hover:text-white cursor-pointer" />
             </button>
-            <h3 className='font-bold text-4xl font-sans text-gray-200'> my portfolio </h3>
+            <h3 className="font-bold text-2xl lg:text-4xl font-sans text-gray-200">
+              {" "}
+              my portfolio{" "}
+            </h3>
+          </div>
+          <div className="md:hidden flex text-4xl px-2 items-center justify-between">
+            <button>
+              <FaBars className="text-white"/>
+            </button>
+            <h3 className="font-bold text-2xl sm:text-4xl font-sans text-gray-200">
+              {" "}
+              my portfolio{" "}
+            </h3>
           </div>
         </div>
-        {/* <div></div> */}
+        <div className="hidden md:flex "></div>
       </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/component/templates/index/header/Header.tsx
+++ src/component/templates/index/header/Header.tsx
@@ -8,10 +8,10 @@ import { FaLinkedin } from "react-icons/fa";
 function Header() {
   return (
     <>
-      <header className="w-full h-screen bg-[url(/images/herosection.jpg)] bg-center bg-cover">
+      <header className="w-full h-screen bg-[url(/images/herosection-mobile.png)] md:bg-[url(/images/herosection.jpg)] bg-center bg-cover">
       <Navbar/>
         <div className="flex items-center justify-between p-2 w-full h-full inset-0">
-          <div className="h-full w-[50%] flex items-center justify-center">
+          <div className="h-full w-[50%] flex items-start  pt-64 lg:pt-0 lg:items-center justify-center">
             <div className="flex flex-col gap-5">
               <div className=" flex items-end justify-center">
                 <div className="flex flex-col items-start justify-center gap-7">
@@ -20,12 +20,12 @@ function Header() {
                     سلام، خوش آمدید من{" "}
                   </h3>
                   <div className="flex items-start justify-start flex-col gap-3">
-                    <h3 className="text-center text-orange-600 text-7xl font-black ">
+                    <h3 className="text-center text-orange-600 text-2xl md:text-5xl xl:text-7xl font-black ">
                       {" "}
                     علیرضا مرادی،
                     {" "}
                     </h3>
-                    <h3 className="text-center text-white text-5xl font-bold">
+                    <h3 className="text-center text-white text-xl md:text-3xl xl:text-5xl font-bold ">
                       توسعه دهنده فرانت اند هستم.
                     </h3>
                   </div>
@@ -33,27 +33,27 @@ function Header() {
               </div>
             </div>
           </div>
-          <div className="h-full w-[40%] flex flex-col items-center justify-end gap-16">
-            <div className="flex flex-col gap-5">
-              <h4 className="text-white font-bold text-3xl leading-10">
+          <div className="h-full w-[40%] flex flex-col items-center justify-end gap-3 lg:gap-16">
+            <div className="flex flex-col gap-2 lg:gap-5">
+              <h4 className="text-white font-bold text-base md:text-lg lg:text-xl xl:text-3xl leading-6 md:leading-10">
                 همیشه در مسیر خلق تجربه‌های سریع و قابل‌اعتماد.
               </h4>
-              <p className="text-white font-extralight">
+              <p className=" font-extralight lg:text-base lg:text-white text-orange-200 text-xs">
                 در دنیای وب‌اپلیکیشن‌ها، من توسعه‌دهنده‌ای هستم که از ساخت هسته‌ی منطقی و قدرتمند پروژه‌ها لذت می‌برم؛ جایی که داده و عملکرد، معنای واقعی می‌گیرند.
               </p>
             </div>
-            <div className="flex flex-wrap items-center justify-center gap-2 w-64">
-              <Link href={"https://t.me/allirezza_mr"} className="group w-28 h-28 rounded-lg bg-blue-600 flex items-center justify-center hover:bg-transparent transition-all">
-                <FaTelegram className="group-hover:text-blue-600 group-hover:scale-150 text-white text-4xl transition-all " />
+            <div className="flex flex-wrap items-center justify-center gap-2 w-32 lg:w-64">
+              <Link href={"https://t.me/allirezza_mr"} className="group w-14 h-14 xl:w-28 xl:h-28 rounded-lg bg-blue-600 flex items-center justify-center hover:bg-transparent transition-all">
+                <FaTelegram className="group-hover:text-blue-600 group-hover:scale-150 text-white text-2xl xl:text-4xl transition-all " />
               </Link>
-              <Link href={"https://t.me/allirezza_mr"} className="group w-28 h-28 rounded-lg bg-red-600 flex items-center justify-center hover:bg-transparent transition-all">
-                <FaInstagramSquare className="group-hover:text-red-500 group-hover:scale-150 text-white text-4xl rounded-lg transition-all  " />
+              <Link href={"https://t.me/allirezza_mr"} className="group w-14 h-14 xl:w-28 xl:h-28 rounded-lg bg-white flex items-center justify-center hover:bg-transparent transition-all">
+                <FaLinkedin className="group-hover:text-blue-600 group-hover:scale-150 text-blue-600 text-2xl xl:text-4xl transition-all" />
               </Link>
-              <Link href={"https://t.me/allirezza_mr"} className="group w-28 h-28 rounded-lg bg-black flex items-center justify-center hover:bg-transparent transition-all">
-                <FaGithub className="group-hover:text-white group-hover:scale-150 text-white text-4xl transition-all " />
+              <Link href={"https://t.me/allirezza_mr"} className="group w-14 h-14 xl:w-28 xl:h-28 rounded-lg bg-black flex items-center justify-center hover:bg-transparent transition-all">
+                <FaGithub className="group-hover:text-white group-hover:scale-150 text-white text-2xl xl:text-4xl transition-all " />
               </Link>
-              <Link href={"https://t.me/allirezza_mr"} className="group w-28 h-28 rounded-lg bg-blue-600 flex items-center justify-center hover:bg-transparent transition-all">
-                <FaLinkedin className="group-hover:text-blue-600 group-hover:scale-150 text-white text-4xl transition-all" />
+              <Link href={"https://t.me/allirezza_mr"} className="group w-14 h-14 xl:w-28 xl:h-28 rounded-lg bg-red-600 flex items-center justify-center hover:bg-transparent transition-all">
+                <FaInstagramSquare className="group-hover:text-red-500 group-hover:scale-150 text-white text-2xl xl:text-4xl rounded-lg transition-all  " />
               </Link>
             </div>
           </div>
