8906aed9dd38cfe38d03bd003cd52165519aeb15
alireza moradi <alirezza.dev2@gmail.com>
2025-12-13 17:27:50 +0330

    01. Adding Dark and Light Mode to the Project

--- src/component/modules/Navber/Navbar.tsx
+++ src/component/modules/Navber/Navbar.tsx
@@ -1,17 +1,46 @@
 "use client";
-import { LuMoonStar } from "react-icons/lu";
+import { LuMoonStar, LuSun } from "react-icons/lu";
 import { FaBars } from "react-icons/fa6";
 import { HiArrowLeftStartOnRectangle } from "react-icons/hi2";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AnimatePresence } from "motion/react";
 import { motion } from "motion/react";
 
 function Navbar() {
+  const [isDark, setIsDark] = useState<boolean>(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const showMenu = (): void => {
     setIsOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    const savedTheme: string | null = localStorage.getItem("theme");
+    const prefersDark: boolean = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    if (savedTheme === "dark" || (!savedTheme && prefersDark)) {
+      document.documentElement.classList.add("dark");
+      setIsDark(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setIsDark(false);
+    }
+  }, []);
+
+  const toggleTheme = (): void => {
+    const newTheme: boolean = !isDark;
+    if (newTheme) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+    setIsDark(newTheme);
+  };
+
   return (
     <>
       <nav className="absolute w-full flex justify-center z-50">
@@ -43,9 +72,20 @@ function Navbar() {
             </li>
           </ul>
           <div className="hidden md:flex items-center justify-center gap-5 xl:gap-28">
-            <button aria-label="Toggle Theme">
+            {/* <button aria-label="Toggle Theme" onClick={toggleTheme}>
               <LuMoonStar className="text-gray-600 text-2xl transition-all hover:text-white cursor-pointer" />
-            </button>
+            </button> */}
+            <AnimatePresence initial={false}>
+              {isDark ? (
+                <motion.button aria-label="Toggle Theme" onClick={toggleTheme}  >
+                  <LuSun className="text-gray-600 text-2xl transition-all hover:text-white cursor-pointer" />
+                </motion.button>
+              ) : (
+                <button aria-label="Toggle Theme" onClick={toggleTheme}>
+                  <LuMoonStar className="text-gray-600 text-2xl transition-all hover:text-white cursor-pointer" />
+                </button>
+              )}
+            </AnimatePresence>
             <h3 className="font-bold text-2xl lg:text-4xl font-sans text-gray-200">
               {" "}
               my portfolio{" "}
@@ -64,14 +104,14 @@ function Navbar() {
         <AnimatePresence>
           {isOpen && (
             <motion.div
-              initial={{ opacity: 0, translateX: "0%" }}
+              initial={{ opacity: 0, translateX: "100%" }}
               animate={{ opacity: 1, translateX: 0 }}
               exit={{ opacity: 0, translateX: 100 }}
               className=" w-full h-screen transition-all"
             >
-              <div className="z-50 flex flex-col items-start justify-between md:hidden absolute right-0 top-0 bottom-0 h-screen w-[60%] bg-bgLight  p-3">
+              <div className="z-50 flex flex-col items-start justify-between md:hidden absolute right-0 top-0 bottom-0 h-screen w-[60%] bg-bgLight dark:bg-gray-800  p-3">
                 <div className="flex w-full justify-between items-center">
-                  <h3 className="text-3xl text-gray-600 font-bold font-serif">
+                  <h3 className="text-3xl text-gray-600 dark:text-gray-400 font-bold font-serif">
                     {" "}
                     my portfolio{" "}
                   </h3>
@@ -129,13 +169,14 @@ function Navbar() {
                 </div>
                 <div className="flex w-full items-center justify-between">
                   <button
-                    className="rounded-full text-center bg-orange-600/60 p-2  text-white cursor-pointer"
+                    className="rounded-full text-center bg-orange-600/60 p-2  text-white dark:text-gray-300 cursor-pointer"
                     aria-label="Theme Toggle"
+                    onClick={toggleTheme}
                   >
                     <LuMoonStar className="text-3xl" />
                   </button>
                   <button
-                    className="rounded-full text-center bg-orange-600/60 p-2  text-white cursor-pointer"
+                    className="rounded-full text-center bg-orange-600/60 p-2  text-white dark:text-gray-300 cursor-pointer"
                     onClick={showMenu}
                     aria-label="Close Menu"
                   >
