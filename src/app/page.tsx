1ab63f302d0af136ca541ff79e22ef64f1068a11
alireza moradi <alirezza.dev2@gmail.com>
2025-12-24 10:53:39 +0330

    01. fixing a bug of github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Footer from "@/component/modules/Footer/Footer";
 import AboutMe from "@/component/templates/index/body/AboutMe";
-import GithubStats from "@/component/templates/index/body/GithubActivity";
-import GithubGrass from "@/component/templates/index/body/GithubActivity";
 import Projects from "@/component/templates/index/body/Projects";
 import Skills from "@/component/templates/index/body/Skills";
 import Header from "@/component/templates/index/header/Header";
+import dynamic from "next/dynamic";
 import React from "react";
+const GithubStats = dynamic(
+  () => import("@/component/templates/index/body/GithubActivity"),
+  { ssr: false }
+);
 
 export default function Home(): React.ReactElement {
   return (
